8e5c33ea0aebed2597524b85fbe745d0877a5cd2
ruekurniawan <ruekurniawan>
2018-12-18 15:34:08 +0700

    Tugas Rabu

---
+++ Selasa/IfElse.js
@@ -0,0 +1,29 @@
+var nama = 'Mikael';
+var peran = 'Ksatria';
+
+if(nama === "")
+{
+	console.log("Nama harus diisi!");
+}
+else
+{
+	if(peran === "Ksatria")
+	{
+		console.log("Selamat datang di Dunia Proxytia, "+nama);
+		console.log("Halo "+peran+" "+nama+" kamu dapat menyerang dengan senjatamu!");
+	}
+	else if(peran === "Tabib")
+	{
+		console.log("Selamat datang di Dunia Proxytia, "+nama);
+		console.log("Halo "+peran+" "+nama+" kamu akan membantu temanmu yang terluka.");
+	}
+	else if(peran === "Penyihir")
+	{
+		console.log("Selamat datang di Dunia Proxytia, "+nama);
+		console.log("Halo "+peran+" "+nama+" ciptakan keajaiban yang membantu kemenanganmu!");
+	}
+	else
+	{
+		console.log("Halo "+nama+", Pilih peranmu untuk memulai game!");		
+	}
+}
