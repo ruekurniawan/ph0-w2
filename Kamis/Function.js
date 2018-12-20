2e235911ecae724e812ec96b40cb95b6e96b6256
ruekurniawan <ruekurniawan>
2018-12-20 19:31:14 +0700

    Tugas Kamis

---
+++ Kamis/Function.js
@@ -0,0 +1,33 @@
+// 1. Tugas 1
+function shoutOut()
+{
+	return "Hallo function";
+}
+console.log(shoutOut());
+
+// 2. Tugas 2
+
+function calculateMultiply(num1, num2)
+{
+	return num1 * num2;
+}
+
+var num1 = 5;
+var num2 = 6;
+
+var hasilPerkalian = calculateMultiply(num1,num2);
+console.log(hasilPerkalian); // Menampilkan angka 30
+
+// 3. Tugas 3
+function processSentence(name,age,address,hobby)
+{
+	return "Nama saya "+name+", umur saya "+age+" tahun, alamat saya di "+address+", dan saya punya hobby yaitu "+hobby+"!";	
+}
+
+var name = "Agus";
+var age = 30;
+var address = "Jln. Malioboro, Yogjakarta";
+var hobby = "gaming";
+
+var fullSentence = processSentence(name,age,address,hobby);
+console.log(fullSentence);
